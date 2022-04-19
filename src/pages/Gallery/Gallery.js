82acac8ee4cf215762a2d2c07fb997c64e513e56
lubava <joheunsaram@mail.ru>
2022-04-19 20:27:32 +0300

    add a photo on page

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -3,18 +3,23 @@ import axios from "axios";
 
 function Gallery() {
   const [items, setItems] = useState([]);
-  let ItemsLength = 24;
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/photos?albumId=1")
       .then((resp) => setItems(resp.data));
+    // .then((resp) => console.log(resp.data));
   });
 
   return (
     <div>
       <div>
         {items.map((item) => (
-          <div>{item.id}</div>
+          <div>
+            <div>{item.id}</div>
+            <div>{item.title}</div>
+            <img src={item.url} />
+            <div>{item.thumbnailUrl}</div>
+          </div>
         ))}
       </div>
     </div>
